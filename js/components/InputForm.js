733ea19e6e3bebfda9e1775e12b023c633596d7c
Daniel Beck <nowseemee@gmail.com>
2016-06-29 22:21:25 +0400

    refactor to stateful component

--- js/components/InputForm.js
+++ js/components/InputForm.js
@@ -1,43 +1,48 @@
 import React from 'react';
 
-const InputForm = (props) => {
-
-  const isEnterKeyUsed = (event) => {
-    const enterKey = 13;
-    if (event.keyCode === enterKey ) {
-      event.preventDefault();
-      if (!event.target.value) {
-        props.updateState({activeWidgetId: 0});
-        return;
+class InputForm extends React.Component {
+  render() {
+    const isEnterKeyUsed = (event) => {
+      const enterKey = 13;
+      if (event.keyCode === enterKey ) {
+        event.preventDefault();
+        if (!event.target.value) {
+          this.props.updateState({activeWidgetId: 0});
+          return;
+        }
+        return true;
       }
-      return true;
-    }
-  };
+    };
 
-  const handleAdd = (event) => isEnterKeyUsed(event) ? props.handleAdd(event) : null;
-  const handleUpdate = (widget, event) => { isEnterKeyUsed(event) ? props.handleUpdate(widget, event) : null };
+    const handleAdd = (event) => isEnterKeyUsed(event) ? this.props.handleAdd(event) : null;
+    const handleUpdate = (widget, event) => { isEnterKeyUsed(event) ? this.props.handleUpdate(widget, event) : null };
 
-  return (
-    <div className="rsk-widget-form">
-      {
-        !!props.state.activeWidgetId
-          ?
-          props.viewer.widgets.edges
-            .filter(edge => edge.node.id === props.state.activeWidgetId )
-            .map(edge => (
-              <textarea
-                defaultValue={edge.node.body}
-                placeholder="Type a message..."
-                key={edge.node.id}
-                onKeyDown={handleUpdate.bind(this, edge.node)}
-              />
-            ))
-          :
-          <textarea placeholder="Type a message..." onKeyDown={handleAdd} />
-      }
-    </div>
-  );
-};
+    this.props.state.activeWidgetId ? setTimeout(()=>this.refs.textareaEdit.focus()) : null;
+
+    return (
+      <div className="rsk-widget-form">
+        {
+          !!this.props.state.activeWidgetId
+            ?
+            this.props.viewer.widgets.edges
+              .filter(edge => edge.node.id === this.props.state.activeWidgetId )
+              .map(edge => (
+                <textarea
+                  ref="textareaEdit"
+                  defaultValue={edge.node.body}
+                  placeholder="Type a message..."
+                  key={edge.node.id}
+                  onKeyDown={handleUpdate.bind(this, edge.node)}
+                />
+              ))
+            :
+            <textarea autoFocus={true} ref="textareaAdd" placeholder="Type a message..." onKeyDown={handleAdd} />
+        }
+      </div>
+    );
+  }
+}
+
 
 
 export default InputForm;

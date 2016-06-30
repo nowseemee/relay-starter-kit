b20f5aa0bab0fc02c172e7ddf519127be7918692
Daniel Beck <nowseemee@gmail.com>
2016-06-30 17:33:20 +0400

    fix first render of date edited

--- js/components/Widget/TimeStamp.js
+++ js/components/Widget/TimeStamp.js
@@ -4,11 +4,8 @@ import React, { Component } from 'react';
 class TimeStamp extends Component {
   componentWillMount() {
     this.createdAgo = moment(this.props.widget.dateCreated).fromNow();
-    this.editedAgo = moment(this.props.widget.dateEdited).fromNow();
-
     this.intervalId = setInterval(() => {
       this.createdAgo = moment(this.props.widget.dateCreated).fromNow();
-      this.editedAgo = moment(this.props.widget.dateEdited).fromNow();
       this.forceUpdate();
     }, 1000 * 60);
   }
@@ -20,7 +17,7 @@ class TimeStamp extends Component {
   render() {
     const componentTitle = this.props.widget.dateEdited ? 'edited' : 'created';
     const iconName = this.props.widget.dateEdited ? 'fa-pencil' : 'fa-paper-plane';
-    const dateToShow = this.props.widget.dateEdited ? this.editedAgo: this.createdAgo;
+    const dateToShow = this.props.widget.dateEdited ? moment(this.props.widget.dateEdited).fromNow(): this.createdAgo;
     return (
       <div className="timestamp">
         <span title={componentTitle}>
